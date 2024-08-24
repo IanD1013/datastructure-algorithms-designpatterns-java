f5baed51fab048320b06f19e91b21d458a466edf
IanD1013 <ian.tung1013@gmail.com>
2024-08-25 01:04:35 +1200

    refactor

--- src/fundamentals/Main.java
+++ src/fundamentals/Main.java
@@ -1,6 +1,5 @@
 package fundamentals;
 
-import java.sql.SQLOutput;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
@@ -9,17 +8,24 @@ public class Main {
     final static byte PERCENT = 100;
 
     public static void main(String[] args) {
-        int principal = (int) readNumber("Principal: ", 1000, 1_000_000);
+        int principal = (int) readNumber("principal: ", 1000, 1_000_000);
         float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 30);
         byte years = (byte) readNumber("Period (Years): ", 1, 30);
 
+        printMortgage(principal, annualInterest, years);
+        printPaymentSchedule(principal, annualInterest, years);
+    }
+
+    private static void printMortgage(int principal, float annualInterest, byte years) {
         double mortgage = calculateMortgage(principal, annualInterest, years);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("--------");
         System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
 
+    private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
         System.out.println();
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
@@ -32,11 +38,13 @@ public class Main {
     public static double readNumber(String prompt, double min, double max) {
         Scanner scanner = new Scanner(System.in);
         double value;
+
         while (true) {
             System.out.print(prompt);
             value = scanner.nextFloat();
-            if (value >= min && value <= max)
+            if (value >= min && value <= max) {
                 break;
+            }
             System.out.println("Enter a value between " + min + " and " + max);
         }
         return value;
@@ -46,17 +54,21 @@ public class Main {
         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
         short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
 
-        return principal
+        double balance = principal
                 * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return balance;
     }
 
     public static double calculateMortgage(int principal, float annualInterest, byte years) {
         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
         short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
 
-        return principal
+        double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return mortgage;
     }
 }

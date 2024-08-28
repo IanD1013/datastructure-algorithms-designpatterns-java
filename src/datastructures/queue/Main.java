673eb2ff5186b96e23f27ccb33a3cd4bed55c5fd
IanD1013 <ian.tung1013@gmail.com>
2024-08-28 19:30:56 +1200

    build priority queue using array

--- src/datastructures/queue/Main.java
+++ src/datastructures/queue/Main.java
@@ -1,16 +1,25 @@
 package datastructures.queue;
 
 import java.util.ArrayDeque;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(10);
-        queue.add(20);
-        queue.add(30);
-        var front = queue.remove();
+//        Queue<Integer> queue = new ArrayDeque<>();
+//        queue.add(10);
+//        queue.add(20);
+//        queue.add(30);
+//        var front = queue.remove();
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        queue.add(5);
+        queue.add(1);
+        queue.add(3);
+
+        while (!queue.isEmpty())
+            System.out.println(queue.remove());
     }
 
     public static void reverse(Queue<Integer> queue) {
